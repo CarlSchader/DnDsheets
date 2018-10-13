249ebf57e6562921fd233646855ad117cfe07670
ZKasica <zkasica@mymail.mines.edu>
2018-10-13 03:36:54 -0400

    Connect backend with MongoDB

--- backend/HTTPServer.js
+++ backend/HTTPServer.js
@@ -1,23 +1,55 @@
-const express = require('express')
-const app = express()
-const port = 3000
-const MessagingResponse = require('twilio').twiml.MessagingResponse;
+const express = require('express');
+const twilio = require('twilio');
+const MessagingResponse = twilio.twiml.MessagingResponse;
+const bodyParser = require('body-parser');
 
+const {Stitch, AnonymousCredential} = require('mongodb-stitch-server-sdk');
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.urlencoded({ extended: false }));
 
 // Twilio stuff
 var accountSid = 'AC9c1efaaec9f54b9372036591dd701f80'; // Your Account SID from www.twilio.com/console
 var authToken = 'e42e490ed559f0d61d30faa0c731136c';
 
-var twilio = require('twilio');
 var client = new twilio(accountSid, authToken);
 
-app.post('/sms', (req, res) => {
-	const twiml = new MessagingResponse();
+// app.post('/sms', (req, res) => {
+// 	const twiml = new MessagingResponse();
+//
+// 	twiml.message('Sup. Test works.');
+//
+// 	res.writeHead(200, {'Content-Type': 'text/xml'});
+// 	res.end(twiml.toString());
+// });
 
-	twiml.message('Sup. Test works.');
+const dbClient = Stitch.initializeDefaultAppClient('dndsheets-lukpd');
+dbClient.auth.loginWithCredential(new AnonymousCredential()).then(user => {
+    console.log("Connected to DB");
 
-	res.writeHead(200, {'Content-Type': 'text/xml'});
-	res.end(twiml.toString());
+
+    // dbClient.close();
+}).catch(err => {
+    console.log(err);
+    dbClient.close();
+})
+
+app.post('/receiveSMS', (req, res) => {
+  console.log("Received request: /" + req.body.Body + "/");
+  if(req.body.Body.toLowerCase() == '?') {
+    const twiml = new MessagingResponse();
+    twiml.message('Available Commands:\n1. List Characters');
+    res.writeHead(200, {'Content-Type': 'text/xml'});
+    res.end(twiml.toString());
+  } else {
+    res.send("Bye");
+  }
+});
+
+app.get('/', (req, res) => {
+  res.send("Hello world");
 });
 
 
