a406f1fb7626bd882aa1ceafea4e4c5b4aa0ae2d
Carl Schader <cschader@factorearth.com>
2018-10-13 14:58:08 -0400

    fun with texts

--- backend/HTTPServer.js
+++ backend/HTTPServer.js
@@ -12,6 +12,16 @@ const port = 3000;
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.json());       // to support JSON-encoded bodies
 
+const attributeSet = new Set(["character_name", "class", "level", "background", "player_name", "race", "alignment",
+	"strength", "dexterity", "constitution", "intelligence", "wisdom", "charisma",
+	"inspiration", "proficiency_bonus", "passive_wisdom",
+	"saving_throw_strength", "saving_throw_dexterity", "saving_throw_constitution", "saving_throw_intelligence", "saving_throw_wisdom", "saving_throw_charisma",
+   	"acrobatics", "animal_handling", "arcana", "athletics", "deception", "history", "insight", "intimidation", "investigation", "medicine", "nature", "perception", "performance", "persuasion", "religion", "sleight_of_hand", "stealth", "survival",
+   	"armour_class", "initiative", "speed", "hit_points",
+   	"CP", "SP", "EP", "GP", "PP",
+   	"personality_traits", "ideals", "bonds", "flaws", "attacks_and_spellcasting", "other_proficiencies_and_languages", "equipment", "features_and_traits"
+]);
+
 // object that is inserted into db
 var dbInsertObject = {};
 
@@ -19,16 +29,88 @@ var dbInsertObject = {};
 var accountSid = 'AC9c1efaaec9f54b9372036591dd701f80'; // Your Account SID from www.twilio.com/console
 var authToken = 'e42e490ed559f0d61d30faa0c731136c';
 
+var twilioSendMessage = function(message, req, res) {
+	const twiml = new MessagingResponse();
+
+	twiml.message(message);
+
+	res.writeHead(200, {'Content-Type': 'text/xml'});
+	res.end(twiml.toString());
+}
+
+var twilioSendValue = async function(characterName, attribute, req, res) {
+	dbClient.callFunction("getValue", [characterName, attribute]).then(result => {
+		if (result == null) {
+			twilioSendMessage("Go to the DnDsheets website to create " + characterName + "!", req, res);
+		}
+		else if (!attributeSet.has(attribute)) {
+			twilioSendMessage(attribute + " is not a valid attribute. To see attributes text: list attributes", req, res);
+		}
+		else if (result[attribute] == "") {
+			twilioSendMessage("This attribute is not set for " + characterName + ".", req, res);
+		}
+		else {
+			twilioSendMessage(result[attribute], req, res);
+		}
+	});
+}
+
+var twilioUpdateValue = async function(characterName, attribute, value, req, res) {
+	dbClient.callFunction("updateAttribute", [characterName, attribute, value]).then(result => {
+		console.log(result);
+		if (result.matchedCount == 0) {
+			twilioSendMessage("Go to the DnDsheets website to create " + characterName + "!", req, res);
+		}
+		else if (!attributeSet.has(attribute)) {
+			twilioSendMessage(attribute + " is not a valid attribute. To see attributes text: list attributes", req, res);
+		}
+		else {
+			twilioSendMessage("Update to " + characterName + " complete.", req, res);
+		}
+	});
+}
+
 var client = new twilio(accountSid, authToken);
 
-// app.post('/sms', (req, res) => {
-// 	const twiml = new MessagingResponse();
-//
-// 	twiml.message('Sup. Test works.');
-//
-// 	res.writeHead(200, {'Content-Type': 'text/xml'});
-// 	res.end(twiml.toString());
-// });
+// Actual event listener
+app.post('/sms', (req, res) => {
+	var request = req.body.Body.toLowerCase().split(" ");
+	console.log(request);
+
+	if (request[0] == '?') {
+		
+	}
+	else if (request.length == 2) {
+		var characterName = request[0];
+		var attribute = request[1];
+		twilioSendValue(characterName, attribute, req, res);
+	}
+	else if (request.length == 4 && request[0] == "set") {
+		var characterName = request[1];
+		var attribute = request[2];
+		var value = request[3];
+		twilioUpdateValue(characterName, attribute, value, req, res);
+	}
+	else {
+		twilioSendValue("Command not recognized.", req, res);
+	}
+});
+
+app.post('/receiveSMS', (req, res) => {
+  console.log("Received request: /" + req.body.Body + "/");
+  if(req.body.Body.toLowerCase() == '?') {
+    const twiml = new MessagingResponse();
+    twiml.message('Available Commands:\n1. List Characters');
+    res.writeHead(200, {'Content-Type': 'text/xml'});
+    res.end(twiml.toString());
+  } else {
+    res.send("Bye");
+  }
+});
+
+// End of Twilio stuff
+
+// Start of MangoDB
 
 const dbClient = Stitch.initializeDefaultAppClient('dndsheets-lukpd');
 dbClient.auth.loginWithCredential(new AnonymousCredential()).then(user => {
@@ -44,17 +126,7 @@ dbClient.auth.loginWithCredential(new AnonymousCredential()).then(user => {
     dbClient.close();
 })
 
-app.post('/receiveSMS', (req, res) => {
-  console.log("Received request: /" + req.body.Body + "/");
-  if(req.body.Body.toLowerCase() == '?') {
-    const twiml = new MessagingResponse();
-    twiml.message('Available Commands:\n1. List Characters');
-    res.writeHead(200, {'Content-Type': 'text/xml'});
-    res.end(twiml.toString());
-  } else {
-    res.send("Bye");
-  }
-});
+
 
 app.post('/submitForm', (req, res) => {
   // const fs = require('fs');
